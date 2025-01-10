c418dcce17fa2583531c060757a3979c765f1cc3
Alexander <avsolovkin@edu.hse.ru>
2025-01-10 15:28:52 +0300

    Initial commit (backend)

---
+++ src/test/java/se/hse/room_25/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.hse.room_25.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
