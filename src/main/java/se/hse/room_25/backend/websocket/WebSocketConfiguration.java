eb426b318480119568a66ae55bebcd241f57998e
Alexander <avsolovkin@edu.hse.ru>
2025-05-08 09:26:51 +0300

    feat(code): Fix jwtToken check for WebSocket

--- src/main/java/se/hse/room_25/backend/websocket/WebSocketConfiguration.java
+++ src/main/java/se/hse/room_25/backend/websocket/WebSocketConfiguration.java
@@ -2,10 +2,12 @@ package se.hse.room_25.backend.websocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import se.hse.room_25.backend.config.JwtInterceptor;
 import se.hse.room_25.backend.service.JwtService;
 
 @Configuration
@@ -14,9 +16,12 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     JwtService jwtService;
 
+    JwtInterceptor jwtInterceptor;
+
     @Autowired
-    public void prepare(JwtService jwtService) {
+    public void prepare(JwtService jwtService, JwtInterceptor jwtInterceptor) {
         this.jwtService = jwtService;
+        this.jwtInterceptor = jwtInterceptor;
     }
 
     @Override
@@ -30,5 +35,10 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setApplicationDestinationPrefixes("/app");
         config.enableSimpleBroker("/topic", "/queue");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtInterceptor);
     }
 }
